b99af6c4c932e73766a702f21cce5285290f78b0
Justin Reagor <justin.reagor@joyent.com>
2017-06-22 16:06:41 -0400

    Terraform should generate TLS certificates

--- certificates.tf
+++ certificates.tf
@@ -1,27 +1,29 @@
-# Generate certificates
 data "template_file" "certificates" {
-    template = "${file("${path.module}/templates/kubernetes-csr.json")}"
-    depends_on = [
-      "triton_machine.controller",
-      "triton_machine.worker",
-      "triton_machine.edge_worker"
-    ]
+  template = "${file("${path.module}/templates/kubernetes-csr.json")}"
+  depends_on = [
+    "triton_machine.controller",
+    "triton_machine.worker",
+    "triton_machine.edge_worker"
+  ]
 
-    vars {
-      # variables must be primitives, neither lists nor maps
-      controller0_ip = "${triton_machine.controller.0.primaryip}"
-      worker0_ip = "${triton_machine.worker.0.primaryip}"
-      edge_worker0_ip = "${triton_machine.edge_worker.0.primaryip}"
-    }
+  vars {
+    controller_ips = "${join(",", formatlist("\"%s\"", triton_machine.controller.ips))}"
+    worker_ips = "${join(",", formatlist("\"%s\"", triton_machine.worker.ips))}"
+    edge_worker_ips = "${join(",", formatlist("\"%s\"", triton_machine.edge_worker.ips))}"
+    etcd_ips = "\"${var.etcd1_ip}\",\"${var.etcd2_ip}\",\"${var.etcd3_ip}\""
+  }
 }
+
 resource "null_resource" "certificates" {
   triggers {
     template_rendered = "${data.template_file.certificates.rendered}"
   }
+
   provisioner "local-exec" {
-    command = "echo '${data.template_file.certificates.rendered}' > ../cert/kubernetes-csr.json"
+    command = "echo '${data.template_file.certificates.rendered}' > cert/kubernetes-csr.json"
   }
+
   provisioner "local-exec" {
-    command = "cd ../cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+    command = "cd cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
   }
 }
